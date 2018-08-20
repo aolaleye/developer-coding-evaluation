58a99c9badfa9df3bebae9684962cab77dbc988d
aolaleye <30451506+aolaleye@users.noreply.github.com>
2018-08-19 23:34:15 -0400

    added divs for containers

--- cardinal-mockup/src/components/Clients.js
+++ cardinal-mockup/src/components/Clients.js
@@ -6,25 +6,27 @@ import skanska from '../assets/logos/logo-skanska.png';
 import pg from '../assets/logos/logo-p&g.png';
 import rackroom from '../assets/logos/logo-rackroom.png';
 
-const clients = [{logo: fifththird, name: "fifth third bank"},
-                 {logo: chhn, name: "the christ hospital health network"},
-                 {logo: chs, name: "carolina healthcare system"},
-                 {logo: skanska, name: "skanska"},
-                 {logo: pg, name: "p&g"},
-                 {logo: rackroom, name: "rack room shoes"}];
+const clients = [{logo: fifththird, name: "fifth third bank", class: "fifththird"},
+                 {logo: chhn, name: "the christ hospital health network", class: "chhn"},
+                 {logo: chs, name: "carolina healthcare system", class: "chs"},
+                 {logo: skanska, name: "skanska", class: "skanska"},
+                 {logo: pg, name: "p&g", class: "pg"},
+                 {logo: rackroom, name: "rack room shoes", class: "rackroom"}];
 
 const Clients = () => (
     <section className="clients">
+    <div className="clients-container">
         <div className="client-info">
             <h3>Our Clients</h3>
-            <p>Over the years, our team has had the distinction of working with a variety of brands in different industries. Here is a short list of some of the have worked with.</p>
+            <p>Over the years, our team has had the distinction of working with a variety of brands in different industries. Here is a short list of some of the names we have worked with.</p>
         </div>
-
+    
         {clients.map((client, i) => (
             <div className="client-logo">
-                <img key={i} src={client.logo} alt={client.name}/>
+                <img key={i} src={client.logo} alt={client.name} className={client.class}/>
             </div>
         ))}
+    </div>
     </section>
 )
  
--- cardinal-mockup/src/components/Solutions.js
+++ cardinal-mockup/src/components/Solutions.js
@@ -5,25 +5,25 @@ import ba from '../assets/icons/icon-ba.svg';
 
 const Solutions = () => (
     <section className="solutions">
+        <div>
+            <article>
+                <img src={bi} alt="logo" />
+                <h3>Business Intelligence</h3>
+                <p>We use technology to turn data into knowledge that can help manage and monitor your business, from anywhere.</p>
+            </article>
 
-        <article>
-            <img src={bi} alt="logo" />
-            <h3>Business Intelligence</h3>
-            <p>We use technology to turn data into knowledge that can help manage and monitor your business, from anywhere.</p>
-        </article>
+            <article>
+                <img src={ad} alt="logo" />
+                <h3>App Development</h3>
+                <p>Our mobile specialists work to create rich, easy-to-use consumer or enterprise experiences for IOS, Android.</p>
+            </article>
 
-        <article>
-            <img src={ad} alt="logo" />
-            <h3>App Development</h3>
-            <p>Our mobile specialists work to create rich, easy-to-use consumer or enterprise experiences for IOS, Android.</p>
-        </article>
-
-        <article>
-            <img src={ba} alt="logo" />
-            <h3>Business Analysis</h3>
-            <p>Great strategy begins with listening. We learn about businesses from the inside out, and then propose improvements.</p>
-        </article>
-        
+            <article>
+                <img src={ba} alt="logo" />
+                <h3>Business Analysis</h3>
+                <p>Great strategy begins with listening. We learn about businesses from the inside out, and then propose improvements.</p>
+            </article>
+        </div>
     </section>
 )
  
